36f0e0ca52e53b58c1ed4bc87b1f88f8ac61273b
Said Torres <sesaid17@hotmail.com>
2023-10-27 18:21:00 -0600

    unfinished

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,20 @@ export function activate(context: vscode.ExtensionContext) {
 			const originalText = editor.document.getText();
 			const selection = editor.selection;
 			const selectedText = editor.document.getText(selection);
-			const range = new vscode.Range(selection.start, selection.end);
+
+			// locate the line where the selected text starts
+			const startLine = editor.document.lineAt(selection.start.line);
+
+			// find all matches of the selected text in the original text and get the lines where they start
+			const matches = [];
+			const regex = new RegExp(selectedText, 'g');
+			let match;
+			while ((match = regex.exec(originalText)) !== null) {
+				const line = editor.document.positionAt(match.index).line;
+				matches.push(line);
+			}
+
+			const matchPosition = matches.indexOf(startLine.lineNumber);
 
 			vscode.workspace.openTextDocument({ content: selectedText, language: editor.document.languageId })
 				.then(doc => {
@@ -22,13 +35,21 @@ export function activate(context: vscode.ExtensionContext) {
 									const edit = new vscode.WorkspaceEdit();
 
 									const newText = event.document.getText();
-									const modifiedText = originalText.replace(selectedText, newText);
+									// We will get all the matches of the selectedText in the originalText, but we only want to replace the selectedText with the newText in the match that is in the same line as the selectedText.
+									let matchCount = -1;
+									let modifiedText = originalText.replace(selectedText, (match) => {
+										matchCount++;
+										if (matchCount === matchPosition) {
+											return newText; // Replace the second match
+										} else {
+											return match; // Keep other matches as they are
+										}
+									});
 
-									// replace all the original text with the modified text
+									console.log(modifiedText);
+
 									const allTextRange = new vscode.Range(new vscode.Position(0, 0), new vscode.Position(editor.document.lineCount + 1, 0));
 									edit.replace(originalUri, allTextRange, modifiedText);
-
-									vscode.workspace.applyEdit(edit);
 								}
 							});
 
